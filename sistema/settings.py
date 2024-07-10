e780aec3236a586d483524f5e0f926022fa90672
FranCaldart <113444581+FranCaldart@users.noreply.github.com>
2024-07-10 16:08:39 -0300

    Update settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -81,15 +81,13 @@ WSGI_APPLICATION = "sistema.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+DATABASE_URL = os.getenv("postgresql://postgres:mhwklDluNXdiKdQwYUwXnaUicWKqSnPr@monorail.proxy.rlwy.net:54043/railway")
 
-DATABASES =  {
-    "default":dj_database_url.config {
-        default : 'postgresql://postgres:mhwklDluNXdiKdQwYUwXnaUicWKqSnPr@monorail.proxy.rlwy.net:54043/railway',
-        conn_max_age = 600,
-        ssl_require = not DEBUG
-        
-    }
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
+
+
 
 
 # Password validation
