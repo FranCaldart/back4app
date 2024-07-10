16d5b485559473343f76d5f50013a159cb8b4f43
FranCaldart <113444581+FranCaldart@users.noreply.github.com>
2024-07-09 22:23:25 -0300

    Update admin.py

--- editais/admin.py
+++ editais/admin.py
@@ -50,7 +50,7 @@ class ItemInline(TabularInline):
 
 class EditalAdmin(ModelAdmin,ImportExportModelAdmin):
     inlines = [ItemInline]
-    list_display = ('municipio', 'data_pregao') 
+    list_display = ('municipio', 'data_pregao','valor_total_display') 
     list_filter_submit = True 
     list_filter = (
         ("data_pregao", DateRangeQuickSelectListFilterBuilder()),
@@ -58,6 +58,9 @@ class EditalAdmin(ModelAdmin,ImportExportModelAdmin):
         )
     import_form_class = ImportForm
     export_form_class = ExportForm 
+    def valor_total_display(self, obj):
+        return obj.valor_total()
+    valor_total_display.short_description = 'Valor Total'
     
 
 class PrecoInline(TabularInline):
