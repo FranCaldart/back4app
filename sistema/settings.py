62c983148a64754dea05dbfa1fb1ca0f5f6b899a
FranCaldart <113444581+FranCaldart@users.noreply.github.com>
2024-07-10 12:01:04 -0300

    Update settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -82,10 +82,12 @@ WSGI_APPLICATION = "sistema.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASES =  {
+    "default":dj_database_url.config {
+        default = 'postgresql://postgres:mhwklDluNXdiKdQwYUwXnaUicWKqSnPr@monorail.proxy.rlwy.net:54043/railway',
+        conn_max_age = 600,
+        ssl_require = not DEBUG
+        
     }
 }
 
